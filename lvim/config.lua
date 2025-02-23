00df0f5f85a29030eb1c3eb0096255671505ce48
Jaskeerat Randhawa <jaskeerat.randhawa@fplabs.tech>
2025-02-23 13:54:48 +0530

    update cofings

--- lvim/config.lua
+++ lvim/config.lua
@@ -8,11 +8,10 @@ lvim.plugins = {
     lazy = false,
   },
   {
-      "supermaven-inc/supermaven-nvim",
-      config = function()
-        require("supermaven-nvim").setup({})
-        require("supermaven-nvim.api").use_free_version()
-      end,
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
   },
   {
     "nvim-telescope/telescope.nvim",
@@ -28,22 +27,63 @@ lvim.plugins = {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000
+    priority = 1000,
+    opts = {
+      term_colors = true,
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false,   -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.95, -- percentage of the shade to apply to the inactive window
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        harpoon = true,
+        telescope = true,
+        mason = true,
+        noice = true,
+        notify = true,
+        which_key = true,
+        fidget = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    },
   },
-  {
-    "leoluz/nvim-dap-go",
-  }
 }
+
 
 ------------------------
 -- Dap
 ------------------------
-local dap_ok, dapgo = pcall(require, "dap-go")
-if not dap_ok then
-  return
-end
+-- local dap_ok, dapgo = pcall(require, "dap-go")
+-- if not dap_ok then
+--   return
+-- end
 
-dapgo.setup()
+-- dapgo.setup()
 
 vim.opt.relativenumber = true -- relative line numbers
 
@@ -53,7 +93,9 @@ lvim.keys.normal_mode["<C-h>"] = "<cmd>TmuxNavigateLeft<cr>"
 lvim.keys.normal_mode["<C-j>"] = "<cmd>TmuxNavigateDown<cr>"
 lvim.keys.normal_mode["<C-k>"] = "<cmd>TmuxNavigateUp<cr>"
 lvim.keys.normal_mode["<C-l>"] = "<cmd>TmuxNavigateRight<cr>"
+lvim.keys.normal_mode["L"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
 lvim.builtin.which_key.setup.plugins.presets.z = true
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.colorscheme = "catppuccin-mocha"
-table.insert(lvim.builtin.cmp.sources,{ name = "supermaven" })
+table.insert(lvim.builtin.cmp.sources, { name = "supermaven" })
